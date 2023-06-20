825c913a6a99d2de527813f0074bc6dfb0295fc3
prithvijedipatil <prithvipatil09@gmail.com>
2023-06-20 15:27:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+
+
+window.onload = () => {
+
+	const item1 = document.getElementById("level");
+	const doml = finddomlevel(item1);
+    console.log("pass");
+	alert(`The level of the element is: ${doml}`);
+};
+
+
+
 //your JS code here. If required.
+function finddomlevel(element) {
+
+	let domcount=0;
+	let currentelement = element;
+	while(currentelement.parentNode){
+		currentelement = currentelement.parentNode;
+		domcount++;
+	}
+
+	return domcount;
+	
+}
